e8e584537e637626e6ca77d69213c4468adbff9c
wuminglan <1037279225@qq.com>
2017-08-19 17:14:53 +0800

    vue_cli构建后完善的项目基架

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<section>
+  <v-header></v-header>
+</section>
+</template>
+
+<script>
+import header from './components/header/header.vue';
+export default {
+ components:{
+   'v-header':header,
+ }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>vue-cli项目的脚本支架</h1>
+</template>
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
